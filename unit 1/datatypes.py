6ea8c4f2a8394c31e3c114a66870e62e7e8eba76
strictly2900 <lbowser426@boyslatin.net>
2026-09-01 14:25:25 +0000

    data type lesson

---
+++ unit 1/datatypes.py
@@ -0,0 +1,35 @@
+# STRING characters that are wrapped within qoutes
+# characters can be letters, numbers, and symbols
+#you can use both single and double
+
+
+"Linwood Bowser"
+
+"123 main street'
+
+"this cost $100 dollars.'
+
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer.
+
+# INTERGERS- datatypes that are just whole numbers
+# without a decimal point. We don't have to add any special symbols.
+# we can write the number as is.
+
+10
+20
+444
+
+# FLOATS- a number with decimal point
+2.9
+67.69
+4.44
+
+# BOOLEANS- a data type that can only be either true or false
+# we do not use quotation marks for boolean values. the computer
+# understand these words as something unique.
+
+
+False 
+
+True
