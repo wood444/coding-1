b0aca5a8a8c7248fb27772aa00630ade7e988cd4
strictly2900 <lbowser426@boyslatin.net>
2026-09-04 14:37:30 +0000

    ai basics work

--- unit 1/variableActivity.py
+++ unit 1/variableActivity.py
@@ -12,6 +12,10 @@ arizona 1.25
 oldtyme 1.75
  half-gallon of lemonade 4.50
 
+# Variable Naming Conventions
+# we use naming conventions because we CANNOT have spaces in
+# variable names
 
-
-
+# camel case - when creating variable names with multiple words,
+# every new word begins with a capital letter
+# the reason why its callede camel case is bc every word
