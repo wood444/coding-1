dc1ea722ae28416b23e07ff1930134dac4e35276
strictly2900 <lbowser426@boyslatin.net>
2026-09-16 14:32:54 +0000

    operatorsActivity classwork

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -27,4 +27,36 @@ print(10 > 1) # greater than operator
 print(10 < 2) # less than operator
 
 # 2 equal signs compare if something is the same
-print("wood" == "wood") # same as
+print("wood" == "wood") # same as (false)
+print("2" == 2) # same as (false)
+print(2.0 == 2) # same as (true)
+
+# not equal is written !=
+# this is to check and filter for values that are not
+# the same
+print(200 != 100) # True- these are not the same
+print(300 != 300) # False- these are the same
+
+
+
+
+# logical operators- compares 2 conditions to check if
+# they are true or false
+# conditions = other operators
+# instead of symbols, we represent these with words:
+# and, or, not
+
+# AND - checks if 2 conditions are true. If yes, the final
+# result is true
+print(3 > 1 and 100 == 50) # this would come out to be true
+
+# OR- checks if only 1 condition is true. If yes,
+# the final result will be true
+print(3 > 1 or 100 == 50)
+
+# NOT
+
+
+
+
+
