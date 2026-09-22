557c078d3968006aa939d8be3f771ee8f7efe27f
strictly2900 <lbowser426@boyslatin.net>
2026-09-22 14:33:41 +0000

    built in functions pt2

---
+++ unit 1/operatorsActivity.py
@@ -0,0 +1,15 @@
+print(200 == 100)
+# false
+print(15 or 18)
+book = 10.99
+tablet = 399.99
+print(book + tablet)
+cart = 98.87
+amountToGetDiscount = 100.00
+print(amountToGetDiscount - cart)
+Shoes = 200.00
+tax = 0.07
+print(shoe * tax)
+GPA = 86
+Recommendation = True
+print(GPA == Recommendation)
---
+++ unit 2/builtinfunctions.py
@@ -0,0 +1,35 @@
+# Functions - simply put; a code block of
+# instructions for computers to follow.
+
+# Built- in Function - a code block of
+# instruction for a computer to follow
+# that was already written for us.
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+print('hello')
+year = input("what year is it? ")
+print(year)
+
+# input() a built in function that allows a user to
+# type in data into the terminal. the data that is passed
+# in will always be treated as a string data type.
+
+# print() a built in function that allows a user to
+# print data out in the terminal.
+
+
+
+
+
+# Data casting functions
+# these are built in (pre-written) functions
+# that change data types from one form into another
+
+#str() - this datacasting function allows you to change
+# any data type into a string
+
+year = 1906
+print("This event took place in "+str(year))
+
+
+
